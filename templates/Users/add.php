1d5536dbff12c438b2f2e16a14ded3b7e02e7804
Jai Earle <jaiearley@hotmail.com>
2021-09-04 11:57:48 +1000

    Completed CMS tutorial

---
+++ templates/Users/add.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\User $user
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Register new account') ?></h4>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="users form content">
+            <?= $this->Form->create($user) ?>
+            <fieldset>
+                <legend><?= __('User Details') ?></legend>
+                <?php
+                    echo $this->Form->control('email');
+                    echo $this->Form->control('password');
+                ?>
+            </fieldset>
+            <?= $this->Form->button(__('Register')) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
+</div>
